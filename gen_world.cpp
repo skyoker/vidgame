c56a2127c2b00167d12954f2ccaac3d6c26d68ae
skyoker <johanskajaa@gmail.com>
2025-10-25 19:41:14 +0000

    init

--- gen_world.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "world_gen.hpp"
-
-int main() {
-    std::cout << "Starting world generation...\n";
-    generate_world();
-    std::cout << "World generation completed.\n";
-    return 0;
-}
